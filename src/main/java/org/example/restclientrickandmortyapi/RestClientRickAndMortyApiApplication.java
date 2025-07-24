de31925c6fbb732e9951f91abb86c8e1e95c4c16
MustafaNahas <99922508+MustafaNahas@users.noreply.github.com>
2025-07-24 10:38:18 +0200

    Initial commit

---
+++ src/main/java/org/example/restclientrickandmortyapi/RestClientRickAndMortyApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.restclientrickandmortyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientRickAndMortyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestClientRickAndMortyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/restclientrickandmortyapi/RestClientRickAndMortyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.restclientrickandmortyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestClientRickAndMortyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
